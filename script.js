f435f3c9da4210c96726231eee50510f453e7721
pablaofficeal <pavlogorosko56@gmail.com>
2024-06-13 12:19:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
     const resultsSection = document.getElementById('resultsSection');
     const resultText = document.getElementById('resultText');
 
-    // Симуляция процесса проверки (в реальности здесь будет интеграция с антивирусным API)
+    // Генерация случайного процента для демонстрации (в реальности здесь будет интеграция с антивирусным API)
+    let virusProbability = Math.floor(Math.random() * 101);
+
+    // Вывод текста в зависимости от процента вероятности вируса
     if (fileInput.files.length > 0) {
-        resultText.textContent = 'Файл проверен, вероятность вируса: 5%';
+        resultText.textContent = `Файл проверен, вероятность вируса: ${virusProbability}%`;
     } else if (urlInput.value) {
-        resultText.textContent = 'Ссылка проверена, вероятность вируса: 10%';
+        resultText.textContent = `Ссылка проверена, вероятность вируса: ${virusProbability}%`;
     } else {
         resultText.textContent = 'Пожалуйста, загрузите файл или вставьте ссылку для проверки.';
     }
